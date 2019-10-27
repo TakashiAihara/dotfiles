c76d545ab650aebf1b9324be5548e1386c6977da
takashiaihara <takashi.aihara.0523@gmail.com>
2019-10-27 23:20:23 +0900

    modify install-mac.sh for synbolic link

--- install-mac.sh
+++ install-mac.sh
@@ -1,33 +1,75 @@
+# !/bin/bash
+
 # zsh
-brew install zsh
-sudo dscl . -create /Users/$USER UserShell `which zsh`
+echo brew list zsh
+brew list zsh
+if [ $? -ne 0 ]
+then 
+	echo brew install zsh
+	brew install zsh
+	echo 'sudo dscl . -create /Users/$USER UserShell `which zsh`'
+	sudo dscl . -create /Users/$USER UserShell `which zsh`
+fi
 
 # zplug
-brew install zplug
+echo brew list zplug
+brew list zplug
+if [ $? -ne 0 ]
+then 
+	echo brew install zplug
+	brew install zplug
+fi
 
 # neovim
-brew install neovim
+echo brew list neovim
+brew list neovim
+if [ $? -ne 0 ]
+then 
+	echo brew install neovim
+	brew install neovim
+fi
 
 # dein.vim
+echo 'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh'
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+echo sh ./installer.sh ~/.cache/dein
 sh ./installer.sh ~/.cache/dein
+echo rm -f installer.sh
 rm -f installer.sh
 
 # fzf
-brew install fzf
-$(brew --prefix)/opt/fzf/install
+echo brew list fzf
+brew list fzf
+if [ $? -ne 0 ]
+then 
+	echo brew install fzf
+	brew install fzf
+	echo '$(brew --prefix)/opt/fzf/install'
+	$(brew --prefix)/opt/fzf/install
+fi
 
 # tmux
-brew install tmux
+echo brew list tmux
+brew list tmux
+if [ $? -ne 0 ]
+then 
+	echo brew install tmux
+	brew install tmux
+fi
 
-# dotfiles
-mv .zshrc_mac ~/.zshrc
-mv .dein ~/
-mv .tmux.conf ~/
-mv .config ~/
+# .dotfiles
+echo ln -snf $(dirname $(realpath $))/.zshrc_mac ~/.zshrc
+ln -snf $(dirname $(realpath $))/.zshrc_mac ~/.zshrc
+echo ln -snf $(dirname $(realpath $))/.dein ~/
+ln -snf $(dirname $(realpath $))/.dein ~/
+echo ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+echo mkdir -p ~/.config
+mkdir -p ~/.config
+echo ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
+ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
 
 # print
-
 git --version
 tmux -V
 zsh --version
