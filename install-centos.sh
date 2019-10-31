ee08cf9e9e7f490fce5d171f8301e5d7adf7e498
root <takashi.aihara.0523@gmail.com>
2019-10-31 12:50:22 +0900

    modify centos installer

--- install-centos.sh
+++ install-centos.sh
@@ -1,17 +1,18 @@
 yum install epel-release -y
 yum --enablerepo=epel install unar -y
 
-# git
+# ============== git ==============
 
-yum remove git -y 
+yum remove -y git*
+yum autoremove -y
 yum install automake libtool ncurses-devel libevent-devel perl-TermReadKey libsecret wget gcc cmake gcc-c++ -y
+yum install -y fuse fuse-devel fuse-libs
 yum install patch -y
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-perl -pe 's/^enabled=1/enabled=0/g' -i /etc/yum.repos.d/ius.repo
-yum install -y git --enablerepo=ius --disablerepo=base,epel,extras,updates
-yum install -y fuse fuse-devel fuse-libs
+yum-config-manager --disable ius
+yum -y install git2u-all --enablerepo=ius
 
-# zsh
+# ============== zsh ==============
 
 yum remove zsh -y
 
@@ -30,38 +31,30 @@ chsh -s $(which zsh)
 
 yum install ctags global -y
 
-mv .zshrc ~/
-
-# zplug
+# ============== zplug ==============
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-# neovim
+# ============== neovim ==============
 
 # yum remove neovim -y
-# 
 # yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 # yum install -y neovim python{2,3}-neovim
-# 
-# mv .config ~/
 
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O /usr/local/bin/nvim
 chmod +x /usr/local/bin/nvim
 
-# dein.vim
+# ============== dein.vim ==============
 
 wget -L https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh 
 sh ./installer.sh ~/.cache/dein
 rm -f installer.sh
 
-mv .dein ~/
-
-# tmux
+# ============== tmux ==============
 
 yum remove tmux -y
 yum autoremove -y
-yum install bison -y
-yum install libevent-devel -y
+yum install -y bison libevent-devel
 git clone https://github.com/tmux/tmux.git
 cd tmux
 sh autogen.sh
@@ -73,19 +66,29 @@ rm -rf tmux
 
 sed -i '1iset-option -g default-shell '$(which zsh)'\n' .tmux.conf
 
-mv .tmux.conf ~/
-
 # fzf
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-cd ~/.fzf
-yes | ./install
+yes | ~/.fzf/install
 
 yum autoremove -y
-cd ~
+yum clean 
+
+# .dotfiles
+echo ln -snf $(dirname $(realpath $))/.zshrc ~/.zshrc
+ln -snf $(dirname $(realpath $))/.zshrc ~/.zshrc
+echo ln -snf $(dirname $(realpath $))/.dein ~/
+ln -snf $(dirname $(realpath $))/.dein ~/
+echo ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+echo mkdir -p ~/.config
+mkdir -p ~/.config
+echo ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
+ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
 
 # print
 
 git --version
 tmux -V
 zsh --version
+~/.fzf/bin/fzf --version
