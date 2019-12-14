626c1e3b82cbfef1bc4ea0f0eb4ba49cfbdeee1e
takashiaihara <takashi.aihara.0523@gmail.com>
2019-12-14 12:42:13 +0000

    ubuntu modify

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,33 +1,32 @@
+apt-get update
+apt-get install -y language-pack-ja
 update-locale LANG=ja_JP.UTF-8
-apt install language-pack-ja
-apt-get install unar wget make gcc automake -y
-apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
+apt-get install -y unar wget make gcc automake
+apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 
 # git
 
-apt-get -y install wget git git-core
+apt-get install -y wget git git-core
 
 # zsh
 
 apt-get remove zsh -y
 
-apt-get install ctags global -y
-apt-get install libncurses5-dev libncursesw5-dev -y
+apt-get install -y ctags global
+apt-get install -y libncurses5-dev libncursesw5-dev
 
 url=`curl https://sourceforge.net/projects/zsh/files/latest/download -s | grep "<a" | perl -pe 's/^.*href="(.*?)".*$/\1/g'`
 wget $url -O `echo $url | perl -pe 's/^.*\/(.*?)\?.*$/\1/'`
 unar `echo $url | perl -pe 's/^.*\/(.*?)\?.*$/\1/'`
 rm -rf  `echo $url | perl -pe 's/^.*\/(.*?)\?.*$/\1/'`
 cd zsh*
-./configure &&make &&make install
+./configure &&make && make install
 cd ..
 rm -rf zsh*
 
 which zsh | tee -a /etc/shells
 perl -pi -e "s;^(`whoami`:.*:).*?$;\1$(which zsh);g" /etc/passwd
 chsh -s $(which zsh)
-
-mv .zshrc  ~/
 
 # zplug
 
@@ -44,22 +43,18 @@ rm -r build/ ; make CMAKE_BUILD_TYPE=Release ; make install
 cd ..
 rm -rf neovim
 
-mv .config ~/
-
 # dein.vim
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm -f installer.sh
 
-mv .dein ~/
-
 # tmux
 
 apt-get remove tmux -y
 
-apt-get install bison -y
-apt-get install libevent-dev -y
+apt-get install -y bison
+apt-get install -y libevent-dev
 git clone https://github.com/tmux/tmux.git
 cd tmux
 sh autogen.sh
@@ -69,9 +64,6 @@ make install
 cd ..
 rm -rf tmux
 
-sed -i '1iset-option -g default-shell '$(which zsh)'\n' .tmux.conf 
-
-mv .tmux.conf ~/
 
 # fzf
 
@@ -79,11 +71,26 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 cd ~/.fzf
 yes | ./install
 
-cd ../
+cd ~-
 
 apt-get autoremove -y
+
+# .dotfiles
+echo ln -snf $(dirname $(realpath $))/.zshrc ~/.zshrc
+ln -snf $(dirname $(realpath $))/.zshrc ~/.zshrc
+echo ln -snf $(dirname $(realpath $))/.dein ~/
+ln -snf $(dirname $(realpath $))/.dein ~/
+echo ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+ln -snf $(dirname $(realpath $))/.tmux.conf ~/
+echo mkdir -p ~/.config
+mkdir -p ~/.config
+echo ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
+ln -snf $(dirname $(realpath $))/.config/nvim ~/.config/
+
+sed -i '1iset-option -g default-shell '$(which zsh)'\n' ~/.tmux.conf 
 
 # print
 git --version
 tmux -V
 zsh --version
+~/.fzf/bin/fzf --version
